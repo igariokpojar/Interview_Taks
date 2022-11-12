3e57dc17998f58183a53571b9f13aded8520308f
Igariok <igariokpojar@yahoo.com>
2022-11-11 18:53:35 -0600

    Initial commit

---
+++ src/array_tasks/First_DuplicateElements.java
@@ -0,0 +1,32 @@
+package array_tasks;
+
+public class First_DuplicateElements {
+
+     /*
+    write a program that can find the first duplicated element from the array
+     */
+
+    public static int firstDuplicatedElement(int[] array) {
+        int firstDuplicated = 0;
+
+        for (int each : array) {
+            int frequency = 0;
+            for (int each2 : array) {
+                if (each == each2)
+                    frequency++;
+            }
+
+            if (frequency > 1) {
+                firstDuplicated = each;
+                break;
+            }
+
+        }
+
+        return firstDuplicated;
+
+    }
+
+
+}
+
